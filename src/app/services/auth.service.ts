62e2476a1eec924630469efea8d82828287bc188
NadiaOutizi <nadiaoutizi2@gmail.com>
2024-05-20 21:10:16 +0100

    n3

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@angular/core';
 import { IUser } from '../interfaces/iuser';
-<<<<<<< HEAD
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
 import { map, switchMap, catchError } from 'rxjs/operators';
-=======
-import { HttpClient } from '@angular/common/http';
-import { Observable, map, switchMap } from 'rxjs';
->>>>>>> 2ba25af9675abac8c3374066d1a87c1c2b000427
 import { Router } from '@angular/router';
 
 @Injectable({
@@ -16,7 +11,6 @@ import { Router } from '@angular/router';
 export class AuthService {
 
   private apiUrl = 'http://localhost:3000/users';
-<<<<<<< HEAD
   public isAuth: boolean = false;
   private authenticatedUser: IUser | null = null;
   userIdKey!: string;
@@ -25,17 +19,10 @@ export class AuthService {
   constructor(private http: HttpClient, private router: Router) { }
 
   SignUp(user: IUser): Observable<IUser> {
-=======
-  public isAuth : boolean = false;
-  constructor( private http : HttpClient , private route : Router) { }
-
-  SignUp( user :IUser ) :Observable<IUser> {
->>>>>>> 2ba25af9675abac8c3374066d1a87c1c2b000427
     return this.getLastUserId().pipe(
       switchMap(lastId => {
         const nextId = lastId + 1;
         user.id = nextId;
-<<<<<<< HEAD
         return this.http.post<IUser>(this.apiUrl, user).pipe(
           catchError(this.handleError)
         );
@@ -60,33 +47,14 @@ export class AuthService {
     );
   }
 
-=======
-        return this.http.post<IUser>(this.apiUrl, user);
-      })
-    );
-  }
-
-  SignIn(username : string , password : string) : Observable<IUser>{
-    return this.http.post<IUser>(`${this.apiUrl}`,{username,password});
-  }
-
-
->>>>>>> 2ba25af9675abac8c3374066d1a87c1c2b000427
   authenticate(username: string, password: string): void {
     this.SignIn(username, password).subscribe(
       (user: IUser) => {
         this.isAuth = true;
-<<<<<<< HEAD
         this.authenticatedUser = user;
         this.router.navigate(['/']);  // Redirect to the root path
         console.log('User authenticated:', user);
         localStorage.setItem('role', user.role);
-=======
-        this.route.navigate(['/companies']);
-        console.log('this is user return true : '+user);
-        console.log('this is isauth : '+ this.isAuth);
-
->>>>>>> 2ba25af9675abac8c3374066d1a87c1c2b000427
       },
       (error) => {
         console.error('Authentication error:', error);
@@ -94,14 +62,11 @@ export class AuthService {
       }
     );
   }
-<<<<<<< HEAD
 
   getAuthenticatedUser(): Observable<IUser | null> {
     return of(this.authenticatedUser);
   }
 
-=======
->>>>>>> 2ba25af9675abac8c3374066d1a87c1c2b000427
   private getLastUserId(): Observable<number> {
     return this.http.get<IUser[]>(this.apiUrl).pipe(
       map(users => {
@@ -109,7 +74,6 @@ export class AuthService {
           return 0;
         }
         const lastUser = users[users.length - 1];
-<<<<<<< HEAD
         return lastUser.id !== undefined ? +lastUser.id : 0;
       }),
       catchError(this.handleError)
@@ -125,13 +89,4 @@ export class AuthService {
     return localStorage.getItem('role');
   }
  
-=======
-        return lastUser.id !== undefined ? +lastUser.id : 0; // Convert to number using unary plus operator
-      })
-    );
-  }
-
-
-
->>>>>>> 2ba25af9675abac8c3374066d1a87c1c2b000427
 }
