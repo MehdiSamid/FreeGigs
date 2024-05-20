6167fae1f06d3fc42a10928bfdcbe782863e6a9d
NadiaOutizi <nadiaoutizi2@gmail.com>
2024-05-20 21:05:32 +0100

    n3

--- src/app/services/freelancer.service.ts
+++ src/app/services/freelancer.service.ts
@@ -1,18 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http'
-<<<<<<< HEAD
 import { Observable, map, switchMap } from 'rxjs';
 import { Freelancer } from '../interfaces/freelancer';
 import { Skills } from '../enums/skills';
 import { IUser } from '../interfaces/iuser';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
-=======
-import { Observable, map } from 'rxjs';
-import { Freelancer } from '../interfaces/freelancer';
-// import { map } from 'rxjs/operators';
-import { Skills } from '../enums/skills';
->>>>>>> 2ba25af9675abac8c3374066d1a87c1c2b000427
 
 @Injectable({
   providedIn: 'root'
@@ -20,11 +13,7 @@ import { Skills } from '../enums/skills';
 export class FreelancerService {
   private apiUrl = 'http://localhost:3000/freelancers';
 
-<<<<<<< HEAD
   constructor(private http: HttpClient,private authService:AuthService,private router: Router) {
-=======
-  constructor(private http: HttpClient) {
->>>>>>> 2ba25af9675abac8c3374066d1a87c1c2b000427
 
    }
 
@@ -36,7 +25,6 @@ export class FreelancerService {
     return this.http.get<Freelancer>(`${this.apiUrl}/${id}`);
   }
 
-<<<<<<< HEAD
   updateprofile(freelancer: Freelancer): Observable<Freelancer> {
     return this.authService.getAuthenticatedUser().pipe(
       switchMap((user: IUser | null) => {
@@ -49,15 +37,6 @@ export class FreelancerService {
     );
   }
 
-=======
-  createFreelancer(freelancer: Freelancer): Observable<Freelancer> {
-    return this.http.post<Freelancer>(this.apiUrl, freelancer);
-  }
-
-  updateFreelancer(freelancer: Freelancer): Observable<Freelancer> {
-    return this.http.put<Freelancer>(`${this.apiUrl}/${freelancer.id}`, freelancer);
-  }
->>>>>>> 2ba25af9675abac8c3374066d1a87c1c2b000427
 
   deleteFreelancer(id: number): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/${id}`);
@@ -77,7 +56,6 @@ export class FreelancerService {
           })
         );
 }
-<<<<<<< HEAD
 getLocations(): Observable<string[]> {
   return this.http.get<Freelancer[]>(this.apiUrl).pipe(
     map((freelancers: Freelancer[]) => {
@@ -102,22 +80,4 @@ getAuthenticatedFreelancer(): Observable<Freelancer> {
     })
   );
 }
-=======
-
-  // searchFreelancers(skills: string, freelancerLocation: string): Observable<Freelancer[]> {
-  //   const params = new HttpParams()
-  //     .set('skills', skills.toLowerCase())
-  //     .set('freelancerLocation', freelancerLocation.toLowerCase());
-
-  //   return this.http.get<Freelancer[]>(this.apiUrl, { params }).pipe(
-  //     map(freelancers => {
-
-
-  //       return freelancers.filter(freelancer => {
-  //         return freelancer.skills.some(skill => skill.toLowerCase().includes(skills.toLowerCase()));
-  //       })
-  //     })
-  //   );
-  // }
->>>>>>> 2ba25af9675abac8c3374066d1a87c1c2b000427
 }
